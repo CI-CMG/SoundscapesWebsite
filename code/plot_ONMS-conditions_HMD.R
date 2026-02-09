dee364588f03f577439d17d6b376985f7e267506
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-02-09 10:03:57 -0800

    FGB01, FK01, FK05, FK06 HMD plots w/ FOIs

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -28,7 +28,7 @@ rm(list=ls())
 
 #SITES ####
 # ONMSsites = c("sb01", "sb03", "hi01", "hi03", "hi04", "hi08", "pm01", "as01", "mb01", "mb02", "oc02", "cb11" )
-ONMSsites = c("as01")
+ONMSsites = c("fk06")
 
 ## directories ####
 outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
