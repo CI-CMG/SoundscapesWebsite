c6efa10d3d20f32d60a9ee59514156eea72eb917
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-02-18 11:02:01 -0800

    remove PACM section from WCR, PIR, and FGBNMS

--- code/summary_ONMS-GCP-all.R
+++ code/summary_ONMS-GCP-all.R
@@ -294,7 +294,7 @@ write.csv(outputONMS, file = paste0(outDirP, "/data_gantt_ONMS-SS-NRS__gantt_", 
 
 
 #option to load the file, if you already ran and just want to plot ####
-#load(paste0(outDirP, "/data_gantt_ONMS-SS-NRS_gantt_2026-02-05.Rda") )
+load(paste0(outDirP, "/data_gantt_ONMS-SS-NRS_gantt_2026-02-05.Rda") )
 
 # GANTT CHART  ####
 ## COLOR ####
