c660faf04d210cbbf678d2e0d873a0f8eeb7424a
Samara Haver <9533474+sahav@users.noreply.github.com>
2026-03-26 00:24:26 +0000

    Updated NRS data plots

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -29,18 +29,17 @@ rm(list=ls())
 
 #SITES ####
 # ONMSsites = c("sb01", "sb03", "hi01", "hi03", "hi04", "hi08", "pm01", "as01", "mb01", "mb02", "oc02")
-# NRSsites oc03 hi00 ci05 sb09 as10 cb11 ch13 fgb06 #Samara edit line 1315
+# NRSsites oc03 hi00 ci05 sb09 as10 cb11 ch13 fgb06 
 
-
-ONMSsites = c("mb01", "mb02", "mb05")
+ONMSsites = c("hi00")
 
 
 ## directories ####
-outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
+#outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
 #outDir   =  "F:/CODE/GitHub/SoundscapesWebsite/" # your local git repo 
 #outDir   =  "/Users/quca3108/SoundscapesWebsite/" # Quincy local git repo
 #outDir = "X:/Emma_Beretta/SoundscapesWebsite/" #for GCP workstation remote desktop Emma
-#outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
+outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
 
 
 outDirG  =  paste0(outDir,"content/resources/") #where save graphics
