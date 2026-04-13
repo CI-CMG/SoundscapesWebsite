1848f97eac5afc538e558e164906671defae423f
Emma Beretta - NOAA Affiliate <emma.beretta@noaa.gov>
2026-04-13 15:28:11 -0400

    adding missing spaces to site

--- code/plot_ONMS-conditions_HMD.R
+++ code/plot_ONMS-conditions_HMD.R
@@ -32,15 +32,15 @@ rm(list=ls())
 # NRSsites "oc03", "hi00","ci05","sb09","as10","cb11","ch13","fgb06" 
 
 
-ONMSsites = c("as10")
+ONMSsites = c("ci01")
 
 
 ## directories ####
-#outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
+outDir   =  "C:/Users/embe5980/SoundscapesWebsite/" # Emma local git repo 
 #outDir   =  "F:/CODE/GitHub/SoundscapesWebsite/" # your local git repo 
 #outDir   =  "/Users/quca3108/SoundscapesWebsite/" # Quincy local git repo
 #outDir = "X:/Emma_Beretta/SoundscapesWebsite/" #for GCP workstation remote desktop Emma
-outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
+#outDir   = "~/GitHub/SoundscapesWebsite/" #GCP WW
 
 
 outDirG  =  paste0(outDir,"content/resources/") #where save graphics
@@ -1381,99 +1381,106 @@ for (uu in 1:length(ONMSsites)) { # uu = 1
   #add ", text = paste("Year:", Year)" to each year geom_line in aes
   #add "name = "Year", " to  scale_fill_manual and scale_color_manual
   #then run code below
-  # 
-  # mallData_recent <- mallData %>%
-  #   filter(Year != oldest_year) %>%
-  #   pivot_wider(names_from = Quantile, values_from = SoundLevel)
-  # 
-  # mallData_oldest <- mallData %>%
-  #   filter(Year == oldest_year) %>%
-  #   pivot_wider(names_from = Quantile, values_from = SoundLevel)
-  # 
-  # pInt = ggplot() +
-  #   #wind model
-  #   geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windUpp,], aes(x = variable, y = value), color = "black", linewidth = 1) +
-  #   geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windLow,], aes(x = variable, y = value), color = "black", linewidth = 1) +
-  #   scale_x_log10(labels = label_number(),limits = (c(10,fqupper)), guide = "axis_logticks") +  # Log scale for x-axis
-  #   
-  #   scale_color_manual(values = rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
-  #   scale_fill_manual(values =  rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
-  #   
-  #   # Add vertical lines at FOIs, label on right side
-  #   geom_vline(data = FOIs, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
-  #   geom_text(data = FOIs, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
-  #   geom_rect(data = FOIs, aes(xmin = FQstart, xmax = FQend, ymin = 30, ymax = 100, text = NULL),
-  #             fill = "gray", alpha = 0.2, # Adjust alpha for transparency
-  #             inherit.aes = FALSE) +  
-  #   
-  #   # Add vertical lines at FOIs, label on left side
-  #   geom_vline(data = FOIsL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
-  #   geom_text(data = FOIsL, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
-  #   
-  #   # Add vertical set dash lines and grey shaded region at FOI ranges
-  #   #geom_vline(data = FOIsRange, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
-  #   #geom_vline(data = FOIsRange, aes(xintercept = FQend, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
-  #   geom_rect(data = FOIsRange, aes(xmin = FQstart, xmax = FQend, ymin = 30, ymax = 100, text = NULL), 
-  #             fill = "gray", alpha = 0.2
-  #             ,  inherit.aes = FALSE)+  
-  #   geom_text(data = FOIsRange, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
-  #   
-  #   # Add vertical set dash lines and grey shaded region at FOI ranges, label on left
-  #   #geom_vline(data = FOIsRangeL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
-  #   #geom_vline(data = FOIsRangeL, aes(xintercept = FQend, color = Label), linetype = "dotdash", color = "red",linewidth = .5) +
-  #   geom_rect(data = FOIsRangeL, aes(xmin = FQstart, xmax = FQend, ymin = 30, ymax = 100, text = NULL), 
-  #             fill = "gray", alpha = 0.2, 
-  #             inherit.aes = FALSE)+  
-  #   geom_text(data = FOIsRangeL, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
-  #   
-  #   # geom_ribbon(data = mallData %>% pivot_wider(names_from = Quantile, values_from = SoundLevel),
-  #   #             aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), alpha = 0.2) +
-  #   
-  #   #for the geom_ribbons below, if data only has one year (ch01 and fk08), comment out the first geom ribbon and change alpha of second from .3 to .1
-  #   geom_ribbon(data = mallData_recent, aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), alpha = 0.1) +
-  #   
-  #   #for the oldest year, make the shading darker since it is hard to see at alpha = .1 for lightblue
-  #   geom_ribbon(data = mallData_oldest, aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), alpha = 0.3) + # High alpha for visibility
-  #   
-  #   #median HMD values- each year
-  #   geom_line(data = mallData[mallData$Quantile == "50%",], aes(x = Frequency, y = SoundLevel, color = Year), linewidth = 2) +
-  #   
-  #   #median HMD values- all data
-  #   geom_line(data = mALL[mALL$Quantile == "50%",], aes(x = Frequency, y = SoundLevel), color = "black", linewidth = 1,
-  #             linetype = "dotted") +
-  #   scale_y_continuous(limits = c(30, 100)) +  # use to manually scale y minimum so vert line labels are visible
-  #   
-  #   # Additional aesthetics
-  #   theme_minimal() +
-  #   labs(
-  #     #title = paste0(toupper(site), "(",siteInfo$`Oceanographic category`, ")"), 
-  #     caption  = caption_text,
-  #     color = legend_label,        #IF biological then change to Year*
-  #     fill = legend_label,        #IF biological then change to Year*
-  #     x = "Frequency Hz",
-  #     y = expression(paste("Sound Levels (dB re 1 ", mu, " Pa"^2, "/Hz)" ) ),
-  #     subtitle = subtitle_text) +
-  #   theme(legend.position = "right",
-  #         plot.caption = ggtext::element_markdown(hjust = 0, size = 12),
-  #         axis.title.x = element_text(size = 14),           # X-axis label size
-  #         axis.title.y = element_text(size = 14),           # Y-axis label size
-  #         axis.text = element_text(size = 14),
-  #         legend.text = element_text(size = 12),
-  #         axis.ticks.length.x = unit(0.25, "cm"), 
-  #         axis.ticks.x = element_line(color = "grey", linewidth = 0.3), 
-  #         axis.line.x = element_line(color = "grey", linewidth = 0.3)    
-  #   ) 
-  # pInt
-  # 
-  # #make plot interactive
-  # interactive_plot <- ggplotly(pInt)
-  # #, tooltip = "text"
-  # 
-  # #remove hover info over grey shading
-  # interactive_plot$x$data[[14]]$hoverinfo <- "skip"
-  # 
-  # 
-  # interactive_plot
+   
+  mallData_recent <- mallData %>%
+    filter(Year != oldest_year) %>%
+    pivot_wider(names_from = Quantile, values_from = SoundLevel)
+
+  mallData_oldest <- mallData %>%
+    filter(Year == oldest_year) %>%
+    pivot_wider(names_from = Quantile, values_from = SoundLevel)
+
+  pInt = ggplot() +
+    #wind model
+    geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windUpp,], aes(x = variable, y = value), color = "black", linewidth = 1) +
+    geom_line(data = mwindInfo[as.character(mwindInfo$windSpeed) == windLow,], aes(x = variable, y = value), color = "black", linewidth = 1) +
+    scale_x_log10(labels = label_number(),limits = (c(10,fqupper)), guide = "axis_logticks") +  # Log scale for x-axis
+    
+    scale_color_manual(name = "Year", values = rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
+    scale_fill_manual(name = "Year", values =  rev(colorRampPalette(c("darkblue", "lightblue"))(length(unique(summary$year))))) +
+    
+    # Add vertical lines at FOIs, label on right side
+    geom_vline(data = FOIs, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
+    geom_text(data = FOIs, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
+    geom_rect(data = FOIs, aes(xmin = FQstart, xmax = FQend, ymin = 35, ymax = 90),
+              fill = "gray", alpha = 0.2) +  # Adjust alpha for transparency
+    
+    # Add vertical lines at FOIs, label on left side
+    geom_vline(data = FOIsL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "black",linewidth = .5) +
+    geom_text(data = FOIsL, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
+    
+    # Add vertical set dash lines and grey shaded region at FOI ranges
+    #geom_vline(data = FOIsRange, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+    #geom_vline(data = FOIsRange, aes(xintercept = FQend, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+    geom_rect(data = FOIsRange, aes(xmin = FQstart, xmax = FQend, ymin = 35, ymax = 90), 
+              fill = "gray", alpha = 0.2)+  # Adjust alpha for transparency
+    geom_text(data = FOIsRange, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 1, hjust = 0.45, size = 4) +
+    
+    # Add vertical set dash lines and grey shaded region at FOI ranges, label on left
+    #geom_vline(data = FOIsRangeL, aes(xintercept = FQstart, color = Label), linetype = "dashed", color = "red",linewidth = .5) +
+    #geom_vline(data = FOIsRangeL, aes(xintercept = FQend, color = Label), linetype = "dotdash", color = "red",linewidth = .5) +
+    geom_rect(data = FOIsRangeL, aes(xmin = FQstart, xmax = FQend, ymin = 35, ymax = 90), 
+              fill = "gray", alpha = 0.2)+  # Adjust alpha for transparency
+    geom_text(data = FOIsRangeL, aes(x = FQstart, y = 40, label = Label), angle = 90, vjust = 0, hjust = 0.5, size = 4) +
+    
+    
+    #median HMD values- each year
+    geom_line(data = mallData[mallData$Quantile == "50%",], aes(x = Frequency, y = SoundLevel, color = Year, fill = Year, text = paste("Year:", Year)), linewidth = 2) +
+    
+    #median HMD values- all data
+    geom_line(data = mALL[mALL$Quantile == "50%",], aes(x = Frequency, y = SoundLevel, fill = Year, text = paste("Year:", Year)), color = "black", linewidth = 1,
+              linetype = "dotted") +
+    
+    
+    # geom_ribbon(data = mallData %>% pivot_wider(names_from = Quantile, values_from = SoundLevel),
+    #             aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year), alpha = 0.2) +
+    
+    #for the geom_ribbons below, if data only has one year (ch01 and fk08), comment out the first geom ribbon and change alpha of second from .3 to .1
+    geom_ribbon(data = mallData %>%
+                  filter(Year != oldest_year) %>%
+                  pivot_wider(names_from = Quantile, values_from = SoundLevel),
+                aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year, color = Year),
+                alpha = 0.1) +
+    
+    #for the oldest year, make the shading darker since it is hard to see at alpha = .1 for lightblue
+    geom_ribbon(data = mallData %>% 
+                  filter(Year == oldest_year) %>% 
+                  pivot_wider(names_from = Quantile, values_from = SoundLevel),
+                aes(x = Frequency, ymin = `25%`, ymax = `75%`, fill = Year, color = Year), 
+                alpha = 0.3) + # High alpha for visibility
+    scale_y_continuous(limits = c(30, NA)) +  # use to manually scale y minimum so vert line labels are visible
+    
+    # Additional aesthetics
+    theme_minimal() +
+    labs(
+      #title = paste0(toupper(site), "(",siteInfo$`Oceanographic category`, ")"), 
+      caption  = caption_text,
+      color = legend_label,        #IF biological then change to Year*
+      fill = legend_label,        #IF biological then change to Year*
+      x = "Frequency Hz",
+      y = expression(paste("Sound Levels (dB re 1 ", mu, " Pa"^2, "/Hz)" ) ),
+      subtitle = subtitle_text) +
+    theme(legend.position = "right",
+          plot.caption = ggtext::element_markdown(hjust = 0, size = 12),
+          axis.title.x = element_text(size = 14),           # X-axis label size
+          axis.title.y = element_text(size = 14),           # Y-axis label size
+          axis.text = element_text(size = 14),
+          legend.text = element_text(size = 12),
+          axis.ticks.length.x = unit(0.25, "cm"), 
+          axis.ticks.x = element_line(color = "grey", linewidth = 0.3), 
+          axis.line.x = element_line(color = "grey", linewidth = 0.3)    
+    ) 
+  pInt
+
+  #make plot interactive
+  interactive_plot <- ggplotly(pInt)
+  #, tooltip = "text"
+
+  #remove hover info over grey shading
+  interactive_plot$x$data[[14]]$hoverinfo <- "skip"
+
+
+  interactive_plot
   
   
   
