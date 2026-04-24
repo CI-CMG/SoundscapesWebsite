b208b9d380cfcceb0933d4dfd0887863d3fb7f86
Samara Haver <9533474+sahav@users.noreply.github.com>
2026-04-24 01:41:35 +0000

    NRS02 plots

--- code/process_MetricstoHrHMD.R
+++ code/process_MetricstoHrHMD.R
@@ -27,12 +27,12 @@ library(devtools)
 # SET UP PARAMS ####
 rm(list=ls()) 
 DC = Sys.Date()
-site  = "NRS12" 
+site  = "NRS02" 
 site = tolower(site) 
 # 
 # #add for NRS
- gcpF = "PMEL_USVI"
- prodName = "VI" 
+ gcpF = "PMEL_OSP"
+ prodName = "NP" 
 
 # LOCAL DATA DIRECTORIES ####
 #dirGCP = paste0( "/Users/quca3108/ONMS/", site,"/") # NCEI GCP min HMD netCDFs
